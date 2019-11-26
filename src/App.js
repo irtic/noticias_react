2df6ecfd6ee32e6a0260ede75b7c2ee150881d49
smunoz <sgmunoz89@gmail.com>
2019-11-26 00:08:31 -0300

    iterando las noticas y llamando componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
 class App extends Component {
 
   state = {
@@ -29,7 +30,9 @@ class App extends Component {
                 titulo = 'Noticias React API'
             />
             <div className="container white contenedor-noticias">
-                
+                <ListaNoticias 
+                    noticias = {this.state.noticas}
+                />
             </div>
         </Fragment>
     );
---
+++ src/components/Noticia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Noticia = () => {
+    return (
+        <div>
+            noticia
+        </div>
+    );
+};
+
+export default Noticia;
