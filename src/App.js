3329eebb42b0390d20b5fc68eaeac9a12416c377
smunoz <sgmunoz89@gmail.com>
2019-11-25 23:49:33 -0300

    consultando la API de noticias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {}
+
+  state = {
+      noticas : []
+  }
+
+   componentDidMount () {
+        this.consultarNoticias()  
+   }
+
+   consultarNoticias = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=d347195b82da488b9b9a198f0a7ad4b2`
+
+        const respuesta = await fetch(url)
+        const noticas = await respuesta.json();
+
+
+        this.setState({
+            noticas : noticas.articles
+        })
+
+   }
   render() {
     return (
         <h1>Noticias API React</h1>
